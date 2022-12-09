523c78ddb48f62e972407df073238f89efbce36b
jwkidd3 <jkidd@kiddcorp.com>
2022-12-08 18:17:26 -0600

    Update list_users.py

--- filter_plugins/list_users.py
+++ filter_plugins/list_users.py
@@ -1,29 +1,11 @@
-def list_users(users_list, key, value=True):
-    ''' List the users that meet some criteria
-    Args:
-        user_list: dictionary of users.
-        key: the key to be filter by.
-        value: the expected value for that key.
-    Returns:
-        The list of names for all the users that met
-        that criteria.
-    '''
-
-    usernames_list = []
-    for user in users_list:
-        if key in user and user[key] == value:
-            usernames_list.append(user['name'])
-
-    return usernames_list
-
-
+#!/usr/bin/python
 class FilterModule(object):
-    ''' Ansible core jinja2 filters '''
-
     def filters(self):
-        ''' Add the filter to the list of filters '''
-        
         return {
-            # Name of the filter : Name of the function to be called
-            'list_users': list_users
+            'a_filter': self.a_filter,
+            'another_filter': self.b_filter
         }
+
+    def a_filter(self, a_variable):
+        a_new_variable = a_variable + ' CRAZY NEW FILTER'
+        return a_new_variable
